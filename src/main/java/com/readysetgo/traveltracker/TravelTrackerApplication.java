67d437f2c154299efc4f371a6c12a61d26028e26
김두열 <enduf768640@gmail.com>
2024-08-22 10:17:39 +0900

    Initial Commit

---
+++ src/main/java/com/readysetgo/traveltracker/TravelTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.readysetgo.traveltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readysetgo/traveltracker/TravelTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readysetgo.traveltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
