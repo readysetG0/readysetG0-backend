0057a4b09cce49aca1f38c15dc22433c88205cb1
tellang <tellang@daum.net>
2024-09-30 23:41:02 +0900

    [FEAT] #12 마커 관련 저장소 생성
    - `MakerRepository` 생성
      - `JpaRepository` 상속
      - 엔티티와 연동된 CRUD 처리

---
+++ src/main/java/com/readysetgo/traveltracker/maker/adapter/out/persistence/MakerRepository.java
@@ -0,0 +1,7 @@
+package com.readysetgo.traveltracker.maker.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MakerRepository extends JpaRepository<MakerJpaEntity, Long> {
+
+}
